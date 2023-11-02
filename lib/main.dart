b3303bdba4d3811efe9a92931b5c0e5958c6eeca
ludoph <phounphonh.ludovic@gmail.com>
2023-11-02 15:51:10 +0100

    Init new lessons

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_app/views/home/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: HomeView(),
+    );
+  }
+}
